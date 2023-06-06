ce78327fe38b2f1dd172bdc94631f8de5a73adf3
karlamieres <karlamieres@gmail.com>
2023-06-05 22:06:41 -0400

    Initial commit

---
+++ assets/js/funciones.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("button-form");
+const form = document.getElementById("form");
+
+const inputName = document.getElementById("exampleInputName1");
+
+function validateForm() {}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
